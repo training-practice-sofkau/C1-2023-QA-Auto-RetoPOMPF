6b726294616c6325a920036a2488b8703012ca75
James Muñoz <jamb1993@gmail.com>
2023-03-17 15:54:25 -0500

    creando estructura del proyecto

---
+++ src/test/java/com/sofkau/setup/ConstanSetup.java
@@ -0,0 +1,7 @@
+package com.sofkau.setup;
+
+public class ConstanSetup {
+
+    public static final String DEMO_QA_URL="https://zonafit.co/";
+
+}
---
+++ src/test/java/com/sofkau/stepdefinitions/StepDefinition.java
@@ -0,0 +1,12 @@
+package com.sofkau.stepdefinitions;
+
+
+import com.sofkau.setup.WebUI;
+import java.util.logging.Logger;
+public class StepDefinition extends WebUI {
+
+    public static Logger LOGGER = Logger.getLogger(String.valueOf(StepDefinition.class));
+
+
+}
+
