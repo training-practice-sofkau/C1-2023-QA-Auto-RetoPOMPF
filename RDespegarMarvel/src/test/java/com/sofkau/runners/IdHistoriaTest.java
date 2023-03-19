a3a5a6cd2c49442db76b55157c02a04d97d640df
alejandraguadir <yolima.guadir@correounivalle.edu.co>
2023-03-19 14:40:07 -0500

    Ajuste de proyecto con un scenario y dos servicios con un scenario

--- RDespegarMarvel/src/test/java/com/sofkau/runners/IdHistoriaTest.java
+++ RDespegarMarvel/src/test/java/com/sofkau/runners/IdHistoriaTest.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        features = {"src/test/resources/features/asistenciadespegar.feature"},
+        features = {"src/test/resources/features/historiaporId.feature"},
         glue = {"com.sofkau.stepdefinitions"},
         publish = true
 )
-public class AsistenciaTest {
+public class IdHistoriaTest {
 }
