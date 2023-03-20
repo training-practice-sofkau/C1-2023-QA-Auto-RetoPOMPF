cbf6565f30b48860925325674f5c3e58ae553230
Ococho <jonathan.vargas85@gmail.com>
2023-03-20 11:58:47 -0500

    💬 Adding `Plan de Calidad` and `Matriz de Riesgos` in a `.xlsx` file

--- Despegar.comAuto/src/test/java/com/sofkau/stepdefs/TrasladosStepDef.java
+++ Despegar.comAuto/src/test/java/com/sofkau/stepdefs/TrasladosStepDef.java
@@ -4,11 +4,23 @@ import com.sofkau.setup.WebUI;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.sofkau.setup.SetUpConst.TRANSFER_URL;
 
 public class TrasladosStepDef extends WebUI {
+    private final Logger log = LoggerFactory.getLogger(TrasladosStepDef.class);
 
     @Given("I went to Despegar transfer page with a browser {string}")
     public void iWentToDespegarTransferPageWithABrowser(String browser) {
+        try {
+            log.info("Init scenario");
+            generalSetup(browser, TRANSFER_URL);
+        } catch (Exception e) {
+            log.error("Wrong Setup provided");
+            quitDriver();
+        }
     }
 
     @Given("I selected the required fields for my family")
