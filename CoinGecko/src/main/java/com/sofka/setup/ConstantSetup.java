8d1e96ccc9e65000a9465b4deaf10591584d29f2
efsolora <efsolora@gmail.com>
2023-03-20 10:13:11 -0500

    Se agregar estructura y contenido para la API

---
+++ CoinGecko/src/main/java/com/sofka/setup/ConstantSetup.java
@@ -0,0 +1,10 @@
+package com.sofka.setup;
+
+public class ConstantSetup {
+    public static final String URLget = "https://api.coingecko.com/api/v3";
+    public static final String COMPLEMENTURL= "/coins/";
+    public static final String COMPLEMENTURLHISTORY = "/history?date=";
+
+
+
+}
