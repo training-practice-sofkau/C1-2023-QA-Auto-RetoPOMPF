243758e707659fab4dc3840cd025b2cc7d0dab8a
Ivan <idruizbe@gmail.com>
2023-03-20 19:52:54 -0500

    Se corrigen los productos a seleccionar

--- POM_Services/src/main/java/com/sofkau/page/FormaDeLaPagina.java
+++ POM_Services/src/main/java/com/sofkau/page/FormaDeLaPagina.java
@@ -21,11 +21,11 @@ public class FormaDeLaPagina extends CommonActionOnPage {
         click(By.xpath("(//a[@class='nav-top-link'])[5]"));
     }
     public void clickProductos() throws InterruptedException {
-        for (int i=3;i<6;i++){
+        for (int i=4;i<7;i++){
             scroll(500);
             click(By.xpath(String.format("(//div[@class='image-fade_in_back'])[%s]",i)));
             scroll(500);
-            if (i==4)
+            if (i==6)
                 click(By.xpath("(//div[@data-value='vanilla'])"));
             else
                 click(By.xpath("(//div[@data-value='banana'])"));
